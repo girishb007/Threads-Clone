3489020616354faa4cbec47593bd2f18357d5db8
Girish Bisane <girishbisane9096@gmail.com>
2023-07-09 21:35:03 +0530

    module exports changes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-// module.exports = {
-//   // other configuration options
-//   experimental: {
-//     appDir: true,
-//   },
-// };
-module.exports 
+module.exports = {
+  // other configuration options
+  experimental: {
+    appDir: true,
+  },
+};
